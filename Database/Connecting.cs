6b88b224a95ab29036dc718b0caef0c5118f7d51
nguyentoan1998 <72169185+nguyentoan1998@users.noreply.github.com>
2022-09-30 13:39:18 +0700

    Add files via upload

--- Database/Connecting.cs
+++ Database/Connecting.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,17 @@ namespace Database
 {
     public class Connecting
     {
-
+        private SqlConnection OpenConnect()
+        {
+            SqlConnection sqlConnection;
+            string Connect = @"Data Source=DESKTOP-9UB1AIQ\TINPHAT;Initial Catalog=TinPhat;Persist Security Info=True;User ID=sa;Password=123456";
+            using (sqlConnection = new SqlConnection(Connect))
+            {
+                if (sqlConnection.State == System.Data.ConnectionState.Closed)
+                    sqlConnection.Open();
+            }
+            return sqlConnection;
+        }
+        public getlist()    
     }
 }
