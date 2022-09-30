8a81b36d4d07746ed48eaea3cf1282050413cb26
nguyentoan1998 <72169185+nguyentoan1998@users.noreply.github.com>
2022-09-30 13:39:38 +0700

    Add files via upload

---
+++ Model/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class User
+    {
+        public string Ma_User { get; set; }
+        public string Ma_NV { get; set; }
+        public string User1 { get; set; }
+        public string Pass { get; set; }
+        public string Permision { get; set; }
+        public System.DateTime Creat_Date { get; set; }
+        public User(string id, string name, string user, string pass, string quyen)
+        {
+            this.Ma_User = id;
+            this.Ma_User = name;
+            this.User1 = user;
+            this.Pass = pass;
+            this.Permision = quyen;
+            this.Creat_Date = DateTime.Now;
+        }
+    }
+}
